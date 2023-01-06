17fec4051effc66c944e739db550b477a368515b
ashishmuley1101 <ashishmuley1101@gmail.com>
2023-01-06 23:35:12 +0530

    Python File readline() Method.

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
 
-#   appending text to Files at the end in Python
+#   Python File readline() Method
 
-# Open a file for appending at the end of the file without truncating it.
-# Creates a new file if it does not exist.
-# syntax : with open("text.txt", "a") as file_name:  where "a" = append
+# The readline() method returns one line from the file.
+# syntax : file.readline(size)
+# size	Optional. The number of bytes from the line to return. Default -1,
+# which means the whole line.
 
-with open("text1.txt", "a") as file2:
+with open("text1.txt", "r") as file2:
 
-    # appending contents to the test2.txt file
-    file2.write('\nappending the new to check the code.')
+    # readline() printing the first line from the file
+    print(file2.readline())
 
+# Output : Created new file.
