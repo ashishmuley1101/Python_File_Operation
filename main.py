fdff5e3eb687951c2435e55e8da0ac4048f27f4c
ashishmuley1101 <ashishmuley1101@gmail.com>
2023-01-06 23:22:41 +0530

    Appending text to Files at the end in Python.

--- main.py
+++ main.py
@@ -1,13 +1,12 @@
 
-#   Writing to Files in Python
+#   appending text to Files at the end in Python
 
-# If we try to open a file that doesn't exist, a new file is created.
-# If a file already exists, its content is erased, and new content is added to the file.
-# syntax : with open("text.txt", "w") as file_name:
+# Open a file for appending at the end of the file without truncating it.
+# Creates a new file if it does not exist.
+# syntax : with open("text.txt", "a") as file_name:  where "a" = append
 
-with open("text1.txt", "w") as file2:
+with open("text1.txt", "a") as file2:
 
-    # write contents to the test2.txt file
-    file2.write('Created new file.')
-    file2.write('\nWelcome here..!')
+    # appending contents to the test2.txt file
+    file2.write('\nappending the new to check the code.')
 
