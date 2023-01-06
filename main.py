413cfc02388ad24fbeb6ad09bf9cc9fba74b9e5b
ashishmuley1101 <ashishmuley1101@gmail.com>
2023-01-06 23:04:38 +0530

    Use of with...open() for automatically close the file without close() method.

--- main.py
+++ main.py
@@ -1,20 +1,12 @@
 
-# Exception Handling in Files using try and catch block
+#    Use of with...open
 
-# If an exception occurs when we are performing some operation with the file, the code exits without closing the file.
-# A safer way is to use a try...finally block.
+# The with...open syntax to automatically close the file.
+# syntax : with open() as file_name
 
-try:
-    # open a file
-    file1 = open("text.txt")  # by default file on read mode open("text.txt", "r")
+with open("text.txt") as file1: # by default file on read mode open("text.txt", "r")
 
     # read the file
     read_content = file1.read()
     print(read_content)
 
-# Closing a file will free up the resources that were tied with the file.
-# It is done using the close() method in Python
-
-finally:  # finally block close the file if exception occur.
-    # close the file
-    file1.close()
