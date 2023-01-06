f758d9eb1e88fa366cf5a15222e92115313c2eaa
ashishmuley1101 <ashishmuley1101@gmail.com>
2023-01-06 23:52:32 +0530

    Python File writelines() Method writing a list datatype.

--- main.py
+++ main.py
@@ -1,14 +1,10 @@
 
-#   Python File readlines() Method
+#   Python File writelines() Method
 
-# Return all lines in the file, as a list where each line is an item in the list object:
-# syntax : file.readlines(hint)
-# hint	Optional. If the number of bytes returned exceed the hint number, no more lines will be returned.
-# Default value is  -1, which means all lines will be returned.
+# The writelines() method writes the items of a list to the file.
+# syntax : file.writelines(list)
 
-with open("text1.txt", "r") as file2:
+with open("text1.txt", "a") as file2:
+    file2.writelines(["\nSee you soon!", "\nOver and out."])  # list datatype
 
-    # readlines() printing the all lines from the file in list datatype.
-    print(file2.readlines())
 
-# Output : ['Created new file.\n', 'Welcome here..!\n', 'appending the new to check the code.']
