35b5fcdd734ecb50df924d88969c1d3144a3ec0a
ashishmuley1101 <ashishmuley1101@gmail.com>
2023-01-07 10:36:19 +0530

    Python File seek() Method.

--- main.py
+++ main.py
@@ -1,19 +1,24 @@
 
-# Python File tell() Method
+# Python File seek() Method
 
-# The tell() method returns the current file position in a file stream.
+# The seek() method sets the current file position in a file stream.
+# The seek() method also returns the new position.
 
-# syntax : file.tell()
+# syntax : file.seek(offset)
+# offset :	Required. A number representing the position to set the current file stream position.
 
 
 with open("text.txt", "r") as file2:
-    print(file2.readline())
-    print(file2.tell())
+
+    # Change the current file position to 5, and return the rest of the line:
+    file2.seek(5)
+    print(file2.readline())  # O/p : me to python^NULNULNUL
+
+    print(file2.seek(4))     # O/p : 4 --> return the file position
 
 
-# Output :
-# welcome to python^NULNULNUL
-#20
+
+
 
 
 
